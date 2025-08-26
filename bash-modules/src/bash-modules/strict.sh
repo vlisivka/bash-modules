3a7394fcb63510ea57f6a48939509c0d07fa4734
Volodymyr M. Lisivka <vlisivka@gmail.com>
2025-08-26 16:00:21 +0300

    Module 'strict' is updated: inherit_errexit added.

--- bash-modules/src/bash-modules/strict.sh
+++ bash-modules/src/bash-modules/strict.sh
@@ -6,13 +6,12 @@
 #>>
 #>>> `strict` - unofficial strict mode for bash
 #>>
-#>> Just import this module, to enabe strict mode: `set -euEo pipefail`.
-#>
-#> ## NOTE
-#>
-#> * Option `-e` is not working when command is part of a compound command,
-#> or in subshell. See bash manual for details. For example, `-e` may not working
-#> in a `for` cycle.
+#>> Just import this module, to enabe strict mode: `set -euEo pipefail`
+#>> and `shopt -s inherit_errexit` .
+#>> Also, it installs handler for ERR trap, to print stack trace on exit
+#>> (requires log module).
 
-set -euEo pipefail
+set -o errexit -o nounset -o errtrace -o pipefail
+shopt -s inherit_errexit
+
 trap 'panic "Uncaught error."' ERR
